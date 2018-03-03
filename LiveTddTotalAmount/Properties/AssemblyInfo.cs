931eb3951369f18296ee7d7d7a45aca38c972524
allyusd <allyusd@yahoo.com.tw>
2018-03-03 16:35:02 +0800

    init: init project and install NSubstitute

---
+++ LiveTddTotalAmount/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiveTddTotalAmount")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("LiveTddTotalAmount")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf172788-590b-445c-a401-49f5f2131551")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
